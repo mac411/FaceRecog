338575857828a6f91ea157b7cd72c0a4aa9ab290
Eddie Xu <eddiexu82@gmail.com>
2020-07-19 17:10:08 -0400

    Update Test_OpenCV.py

--- Test_OpenCV.py
+++ Test_OpenCV.py
@@ -1,2 +1,11 @@
-def test_function():
-    return 1
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
